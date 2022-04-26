b969918cac3883bae37d014d2fac9920fce4ff53
Cristhian Rene Motoche Guerrero <cristhian_mgsys@hotmail.com>
2022-04-25 21:56:37 -0500

    Examen Cristhian Motoche

---
+++ CristhianMotocheExamen/Resumen.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CristhianMotocheExamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Resumen : ContentPage
+    {
+        public Resumen()
+        {
+            InitializeComponent();
+        }
+
+        public Resumen(string strUser, string strNombre, decimal decTotal) 
+        {
+            InitializeComponent();
+
+            LblUser.Text = strUser;
+
+            TxtNombre.Text = strNombre;
+
+            TxtTotal.Text = decimal.Round(decTotal,2).ToString();
+        }
+    }
+}
